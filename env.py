6baa2502a2d5d1a65318ea5a88ff1b5c59199cca
samayjindal <samayjindal84@gmail.com>
2026-04-08 22:56:00 +0530

    Update env.py

--- env.py
+++ env.py
@@ -10,11 +10,15 @@ class Action(BaseModel):
 
 tasks = [
     {"query": "I need money help", "answer": "Kisan Credit Card"},
-    {"query": "My crop got damaged due to rain", "answer": "Crop Insurance"},
-    {"query": "I have small land and need financial support", "answer": "PM Kisan"}
+    {"query": "My crop got damaged", "answer": "Crop Insurance"},
+    {"query": "I need financial support", "answer": "PM Kisan"}
 ]
 
 current_task = {}
+
+@app.get("/")
+def home():
+    return {"status": "running"}
 
 @app.post("/reset")
 def reset():
@@ -26,12 +30,6 @@ def reset():
 def step(action: Action):
     correct = current_task["answer"]
     reward = 1 if action.scheme.lower() == correct.lower() else -1
-
-    if len(action.explanation.split()) <= 12:
-        reward += 0.5
-
-    if "farmer" in action.explanation.lower():
-        reward += 0.2
 
     return {
         "observation": {"query": current_task["query"]},
